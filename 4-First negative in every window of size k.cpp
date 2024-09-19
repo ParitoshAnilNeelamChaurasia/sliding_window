9101eb0cc92185c2b6095e34e3f9a3aba02b0b49
ParitoshAnilNeelamChaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2024-09-19 16:32:58 +0530

    Update 4-First negative in every window of size k.cpp

--- 4-First negative in every window of size k.cpp
+++ 4-First negative in every window of size k.cpp
@@ -1,55 +1,59 @@
-//{ Driver Code Starts
+// https://www.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
+
 #include <bits/stdc++.h>
 using namespace std;
-#define ll  long long 
+#define ll long long
 
-vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K) {
-                vector<ll> ans ;
-                deque<ll> dq ;
-                
-                int i = 0 ; 
-                int j = 0 ;
-                
-                while(j < N)
-                {
-                    if(A[j] < 0)
-                    {
-                        dq.push_back(A[j]) ;
-                    }
-                    
-                    if(j - i + 1 == K)
-                    {
-                        // checking window and filling negative number
-                        if(!dq.empty())
-                        {
-                            ans.push_back(dq.front()) ;
-                        }
-                        else
-                        {
-                            ans.push_back(0) ;
-                        }
-                        
-                        // shifting window
-                        
-                        if(A[i] < 0 && !dq.empty())
-                        {
-                            dq.pop_front() ;
-                        }
-                        i++ ;
-                    }
-                    j++ ;
-                }
-                return ans ;
- }
+vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K)
+{
+    vector<ll> ans;
+    deque<ll> dq;
 
-int main() {
+    int i = 0;
+    int j = 0;
+
+    while (j < N)
+    {
+        if (A[j] < 0)
+        {
+            dq.push_back(A[j]);
+        }
+
+        if (j - i + 1 == K)
+        {
+            // checking window and filling negative number
+            if (!dq.empty())
+            {
+                ans.push_back(dq.front());
+            }
+            else
+            {
+                ans.push_back(0);
+            }
+
+            // shifting window
+
+            if (A[i] < 0 && !dq.empty())
+            {
+                dq.pop_front();
+            }
+            i++;
+        }
+        j++;
+    }
+    return ans;
+}
+
+int main()
+{
     long long int t, i;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         long long int n;
         cin >> n;
         long long int arr[n];
-        for (i = 0; i < n; i++) 
+        for (i = 0; i < n; i++)
         {
             cin >> arr[i];
         }
@@ -57,10 +61,9 @@ int main() {
         cin >> k;
 
         vector<long long> ans = printFirstNegativeInteger(arr, n, k);
-        for (auto it : ans) cout << it << " ";
+        for (auto it : ans)
+            cout << it << " ";
         cout << endl;
     }
     return 0;
 }
-
-
